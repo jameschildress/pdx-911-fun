542b73a6d2108c6e15f21e63ab5a902b30e055c2
James Childress <james.childress@me.com>
2013-12-22 21:27:34 -0800

    better color choices

--- draw.rb
+++ draw.rb
@@ -11,29 +11,32 @@ Bounds = Struct.new(:min_x, :min_y, :max_x, :max_y) do
   end
 end
 
-height = 500
+height = 600
 width  = 1000
+radius = 3
 
 colors = %w(
-  #00F
-  #0F0
-  #F00
-  #FF0
-  #0FF
-  #F0F
-  #000
-  #000
-  #000
-  #000
-  #000
+  #00ffff
+  #8800ff
+  #0000ff
+  #ff00ff
+  #ff0000
+  #ffff00
+  #00ff00
+  #ff8800
+  #0088ff
+  #ff0088
+  #ff8888
+  #88ff88
+  #8888ff
 )
 
 
 
-pixels = PDX911::Database.query("SELECT agency_id AS a , location[0] AS x, location[1] AS y FROM dispatches ORDER BY date DESC")
+pixels = PDX911::Database.query("SELECT agency_id AS a , location[0] AS y, location[1] AS x FROM dispatches ORDER BY date DESC")
 
 pixels.map! do |pix|
-  Point.new pix['x'].to_f, pix['y'].to_f, colors[pix['a'].to_i]
+  Point.new pix['x'].to_f, pix['y'].to_f, pix['a'].to_i
 end
 
 
@@ -48,18 +51,14 @@ end
 
 
 
-canvas = Magick::Image.new(width, height)
+canvas = Magick::Image.new(width, height, Magick::GradientFill.new(0, 0, 0, 0, '#000', '#000'))
 gc = Magick::Draw.new
 
-
-gc.fill '#000'
-
-gc.rectangle(0, 0, width, height) 
-
-pixels.map do |pix|
-  gc.fill pix.color
+pixels.each do |pix|
   x = (pix.x - bounds.min_x) / bounds.x * width
   y = (pix.y - bounds.min_y) / bounds.y * height
+  # gradient_image = Magick::Image.new(width, height, Magick::GradientFill.new(x1, y1, x2, y2, start_color, end_color))
+  gc.fill colors[pix.color]
   gc.point x, y
 end
 
